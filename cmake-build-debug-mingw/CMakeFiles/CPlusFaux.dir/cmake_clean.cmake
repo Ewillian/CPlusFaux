b04b78e0e8fef99d6960dce40d41261edc647a34
Ewillian <guillaume.lecoq@ynov.com>
2020-03-06 15:19:16 +0100

    Init

---
+++ cmake-build-debug-mingw/CMakeFiles/CPlusFaux.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPlusFaux.dir/main.cpp.obj"
+  "CPlusFaux.exe"
+  "CPlusFaux.exe.manifest"
+  "CPlusFaux.pdb"
+  "libCPlusFaux.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPlusFaux.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
